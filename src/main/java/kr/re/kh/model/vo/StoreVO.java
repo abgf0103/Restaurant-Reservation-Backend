d23791d797f7a8a3d94e01637d8658e550f510a6
Jong1 <abgf0103@gmail.com>
2024-11-07 17:51:25 +0900

    fix : store MVC

--- src/main/java/kr/re/kh/model/vo/StoreVO.java
+++ src/main/java/kr/re/kh/model/vo/StoreVO.java
@@ -5,16 +5,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class StoreVO {
-    private Long storeId;       //가게 고유ID
-    private Long userId;        //가게 관리자 ID
-    private String storeName;   //가게명
-    private String address;     //주소
-    private String storeHours;  //영업시간
-    private String storeStatus; //가게 상태 (준비중, 영업중)
-    private String phone;       //가게 연락처
+    private Long storeId;               //가게 고유ID
+    private Long userId;                //가게 관리자 ID
+    private String storeName;           //가게명
+    private String address;             //주소
+    private String storeHours;          //영업시간
+    private String storeStatus;         //가게 상태 (준비중, 영업중)
+    private String phone;               //가게 연락처
+    private LocalDateTime createdAt;    //등록 시간
+    private LocalDateTime updatedAt;    //수정 시간
 }

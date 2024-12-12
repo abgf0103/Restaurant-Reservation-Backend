ef1d6346feedb56af646ee4119bc11e7df2ff6e8
LeeJW98 <tst0579@gmail.com>
2024-12-12 13:39:08 +0900

    username

--- src/main/java/kr/re/kh/model/vo/ReservationVO.java
+++ src/main/java/kr/re/kh/model/vo/ReservationVO.java
@@ -12,13 +12,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class ReservationVO {
-    private Long reserveId;        // 예약 ID
-    private Long userId;           // 사용자 ID
-    private Long storeId;          // 가게 ID
-    private int partySize;         // 인원 수
-    private int reserveStatus;  // 예약 상태
-    private LocalDateTime reserveDate; // 예약 날짜
-    private String storeName;      // 가게 이름
+    private Long reserveId;             // 예약 ID
+    private Long userId;                // 예약한 사용자의 ID
+    private Long storeId;               // 가게 ID
+    private String username;            // 예약자 이름
+    private int partySize;              // 인원 수
+    private int reserveStatus;          // 예약 상태
+    private LocalDateTime reserveDate;  // 예약 날짜
+    private String storeName;           // 가게 이름
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;  // CREATED_AT
